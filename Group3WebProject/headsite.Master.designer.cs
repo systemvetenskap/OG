0f3227a0ee10fb72b5d41797915d06d62ae1212c
ansv <ansv1410@student.miun.se>
2015-10-26 21:59:01 +0100

    Fixat lite finare style på hemsidan. Även lagt till att man kommer till "deafult.aspx" när man trycker på "JE-Banken". Lagt till en login.aspx. Gjort en footer. Ska fixa med login-funktion imorgon. Gonatt

--- Group3WebProject/headsite.Master.designer.cs
+++ Group3WebProject/headsite.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Group3WebProject {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// login control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor login;
+        
+        /// <summary>
         /// body control.
         /// </summary>
         /// <remarks>
