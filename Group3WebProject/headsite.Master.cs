e3cc7c55c21b5b510371e9fdf9c5b6d765bf7f78
ansv <ansv1410@student.miun.se>
2015-10-27 15:41:11 +0100

    pushar det jag gjort för att alan ska kunna göra sin grej

--- Group3WebProject/headsite.Master.cs
+++ Group3WebProject/headsite.Master.cs
@@ -5,14 +5,44 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Xml;
+using System.Web.Configuration;
+using Npgsql;
+using System.Configuration;
 
 namespace Group3WebProject
 {
     public partial class headsite : System.Web.UI.MasterPage
     {
+
+        Classes.clsUsers user;
         protected void Page_Load(object sender, EventArgs e)
         {
+            string connectionString = WebConfigurationManager.ConnectionStrings["JE"].ConnectionString;
+            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
 
+            NpgsqlConnection connection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["JE"].ConnectionString);
+           
+            try
+            {
+                string sql = "SELECT * FROM users";
+
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                
+                //while
+                //{
+                //    user = new Classes.clsUsers();
+                //}
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
         }
 
     }
