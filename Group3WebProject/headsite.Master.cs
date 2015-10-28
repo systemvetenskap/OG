58f12c47ed9f7b346787acb405371c324b586ecb
mNesterud <mnesterud@gmail.com>
2015-10-28 11:19:27 +0100

    småfix med inlogg

--- Group3WebProject/headsite.Master.cs
+++ Group3WebProject/headsite.Master.cs
@@ -17,51 +17,68 @@ namespace Group3WebProject
 
         clsUsers user;
         List<clsUsers> userList = new List<clsUsers>();
+        clsUsers sessionUser;
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            string connectionString = WebConfigurationManager.ConnectionStrings["JE"].ConnectionString;
-            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
-
-            NpgsqlConnection connection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["JE"].ConnectionString);
-            
-
-            try
+            if (!IsPostBack)
             {
-                string sql = "SELECT * FROM users";
-                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
-                conn.Open();
-                NpgsqlDataReader dr = cmd.ExecuteReader();
-                
 
-                while (dr.Read())
+
+                string connectionString = WebConfigurationManager.ConnectionStrings["JE"].ConnectionString;
+                NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+
+                NpgsqlConnection connection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["JE"].ConnectionString);
+
+
+                try
                 {
+                    string sql = "SELECT * FROM users";
+                    NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                    conn.Open();
+                    NpgsqlDataReader dr = cmd.ExecuteReader();
 
-                    user = new Classes.clsUsers();
-                    user.Id = int.Parse(dr["id"].ToString());
-                    user.FirstName = dr["first_name"].ToString();
-                    user.LastName = dr["last_name"].ToString();
-                    user.TeamId = int.Parse(dr["team_id"].ToString());
 
-                    userList.Add(user);
+                    while (dr.Read())
+                    {
+
+                        user = new Classes.clsUsers();
+                        user.Id = int.Parse(dr["id"].ToString());
+                        user.FirstName = dr["first_name"].ToString();
+                        user.LastName = dr["last_name"].ToString();
+                        user.TeamId = int.Parse(dr["team_id"].ToString());
+
+                        userList.Add(user);
+
+                    }
+
+                    
+                    ddl_users.DataSource = userList;
+                    ddl_users.DataBind();
 
                 }
 
-                ddl_users.DataSource = userList;
-                ddl_users.DataBind();
+                catch (Exception ex)
+                {
+                    Response.Write(ex.Message);
+                }
 
+                finally
+                {
+                    conn.Close();
+                }
             }
+        }
 
-            catch (Exception ex)
-            {
-                Response.Write(ex.Message);
-            }
+        protected void ddl_users_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sessionUser = new clsUsers();
+            sessionUser = ddl_users.SelectedItem.Attributes;
 
-            finally
-            {
-                conn.Close();
-            }
+
+                //[ddl_users.SelectedIndex];
+            
         }
 
     }
