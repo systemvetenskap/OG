685b5b9801196e94d9a8869b1e8e5ea153b629e9
ansv <ansv1410@student.miun.se>
2015-10-30 10:39:55 +0100

    Tagit bort dropdwnlist från header och även kommenterat bort all kod därifrån.

--- Group3WebProject/headsite.Master.designer.cs
+++ Group3WebProject/headsite.Master.designer.cs
@@ -31,15 +31,6 @@ namespace Group3WebProject {
         protected global::System.Web.UI.HtmlControls.HtmlAnchor login;
         
         /// <summary>
-        /// ddl_users control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddl_users;
-        
-        /// <summary>
         /// bodyn control.
         /// </summary>
         /// <remarks>
